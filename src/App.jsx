15508835e561024e32da00ebaf96d0ba143bc462
guxtavinho69 <gustavoruiz637@gmail.com>
2022-08-26 13:27:56 -0400

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+//App.jsx
+import './App.css'
+
+function App(){
+return(
+  <div>
+    <div className='main'>
+        <div className='inform'>
+          <div className='title'>
+            Jovens são encontrados sendo gays 
+          </div>
+          <div className='news'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, voluptas recusandae obcaecati impedit soluta rerum, molestiae repudiandae dolorum, fugiat nihil excepturi voluptatum ut dolore minus facere. Harum nemo odit fuga.
+            </div>
+        </div>
+        <div className='mainImg'>
+            <img className='topimg' src='src\1140x641.jpg'/>
+        </div>
+      </div>
+      <div className='sugest'>
+        <div className='box'>
+          <div className='secImg'>
+            <img className='botImg' src='src\1140x641.jpg' />
+          </div>
+          <div className='title2'>
+            segunta n teve aula kkk
+          </div>
+        </div>
+        <div className='box'>
+          <div className='secImg'>
+            <img className='botImg' src='src\1140x641.jpg' />
+          </div>
+          <div className='title2'>
+            alunos enrolam 30 minutos na aula 
+          </div>
+        </div>
+        <div className='box'>
+          <div className='secImg'>
+            <img className='botImg' src='src\1140x641.jpg' />
+          </div>
+          <div className='title2'>
+            tchuru buru tchugus 
+          </div>
+        </div>
+        <div className='box'>
+          <div className='secImg'>
+            <img className='botImg' src='src\1140x641.jpg' />
+          </div>
+          <div className='title2'>
+            sadifh uhdf shd uods fygdi  eufhg  fhs fub sduif 
+          </div>
+        </div>
+        <div className='box'>
+          <div className='secImg'>
+            <img className='botImg' src='src\1140x641.jpg' />
+          </div>
+          <div className='title2'>
+            sadifh uhdf shd uods fygdi  eufhg  fhs fub sduif 
+          </div>
+        </div>
+      </div>
+  </div>
+)
+}
+export default App
